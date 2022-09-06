a4229fb4563e627e3a11753074c2604a6d8211e7
carbon-hvze <6391474+carbon-hvze@users.noreply.github.com>
2022-09-06 13:58:34 +0300

    fix tests, bump zen, prep ci

--- Makefile
+++ Makefile
@@ -1,2 +1,10 @@
+.PHONY: test
+
 repl:
 	clj -M:nrepl:test
+
+test:
+	clj -A:test:kaocha
+
+test-ci:
+	clojure -A:test:kaocha
--- test/zen/http/formats_test.clj
+++ test/zen/http/formats_test.clj
@@ -17,16 +17,18 @@
                       :engine zen.http/formats-middleware
                       :formats #{zen.http/json zen.http/yaml zen.http/transit zen.http/html}}})
 
+  (t/is (empty? (zen/errors ztx)))
+
   (def fmt-mw (zen/get-symbol ztx 'myapp/formats))
 
   (meth/middleware-in ztx fmt-mw {:params {:_format "json"}
                                   :body "{\"a\": 1}"})
 
   (meth/middleware-out ztx fmt-mw {:params {:_format "json"}
-                                   :body {:a 1}})
+                                   :body {:a 1}}
+                       {})
 
   ;; (sys/send ztx 'example/web 'web/dispatch {:uri "/Patient"})
   ;; (sys/send ztx 'example/web 'web/rpc {:method 'example/pt-search :params {}})
-
 
   )
